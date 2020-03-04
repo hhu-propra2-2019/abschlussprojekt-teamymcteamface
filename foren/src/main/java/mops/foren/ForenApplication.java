fea7f6f4e4048b584347e20abf733ca922303582
Jascha <jascha.wolter@web.de>
2020-03-04 10:21:15 +0100

    Improve CI

--- foren/src/main/java/mops/foren/ForenApplication.java
+++ foren/src/main/java/mops/foren/ForenApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ForenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ForenApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ForenApplication.class, args);
+    }
 
 }
