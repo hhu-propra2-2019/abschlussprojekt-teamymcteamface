203ad0bfc17f966ee191df4e5199ac61a68765da
maistai <49523050+maste150hhu@users.noreply.github.com>
2020-03-10 08:59:10 +0100

    Update Admin.java

--- src/main/java/mops/foren/domainmodel/Admin.java
+++ src/main/java/mops/foren/domainmodel/Admin.java
@@ -10,8 +10,4 @@ public class Admin implements Role {
     public boolean hasPermission(Permission permission) {
         return permissions.contains(permission);
     }
-
-    public boolean hasPermissions(Collection<Permission> permissions) {
-        return this.permissions.containsAll(permissions);
-    }
 }
