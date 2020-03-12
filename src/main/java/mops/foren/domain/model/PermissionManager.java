685a48a3efb647d575a2155a2d150f7fbfbbacb7
maste150hhu <mastremolten2@gmail.com>
2020-03-12 22:04:56 +0100

    fix bug in logic

--- src/main/java/mops/foren/domain/model/PermissionManager.java
+++ src/main/java/mops/foren/domain/model/PermissionManager.java
@@ -1,31 +1,29 @@
 package mops.foren.domain.model;
 
-import java.util.List;
+import java.util.Set;
 
 public class PermissionManager {
 
-    private List<ForumId> moderator;
-    private List<ForumId> admin;
+    private Set<ForumId> moderator;
+    private Set<ForumId> admin;
 
     /**
      * Method checks if the permission requires a certain role and
      * also if the user has the required role.
-     * @param id - id of the Forum in which the request was executed
+     *
+     * @param id         - id of the Forum in which the request was executed
      * @param permission - the event represented by it permission-type
      * @return boolean
      */
     public boolean checkPermission(Id id, Permission permission) {
 
-        if (Moderator.hasPermission(permission)) {
-            return moderator.contains(id);
+        if (moderator.contains(id)) {
+            return Moderator.hasPermission(permission);
         }
-        if (Admin.hasPermission(permission)) {
-            return admin.contains(id);
+        if (admin.contains(id)) {
+            return Admin.hasPermission(permission);
         }
+
         return false;
-
     }
-
-
-
 }
