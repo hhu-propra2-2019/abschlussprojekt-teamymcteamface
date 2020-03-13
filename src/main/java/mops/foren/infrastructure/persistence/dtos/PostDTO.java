a347213a66f1ce7132fa051646e1f0b0ff5a3566
Jascha <jascha.wolter@web.de>
2020-03-13 14:16:21 +0100

    Add builder-annotation to dtos

--- src/main/java/mops/foren/infrastructure/persistence/dtos/PostDTO.java
+++ src/main/java/mops/foren/infrastructure/persistence/dtos/PostDTO.java
@@ -1,13 +1,19 @@
 package mops.foren.infrastructure.persistence.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 @Table(name = "post")
 public class PostDTO {
     @Id
--- src/main/java/mops/foren/infrastructure/persistence/dtos/TopicDTO.java
+++ src/main/java/mops/foren/infrastructure/persistence/dtos/TopicDTO.java
@@ -1,12 +1,18 @@
 package mops.foren.infrastructure.persistence.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.Set;
 
-@Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 @Table(name = "topic")
 public class TopicDTO {
     @Id
