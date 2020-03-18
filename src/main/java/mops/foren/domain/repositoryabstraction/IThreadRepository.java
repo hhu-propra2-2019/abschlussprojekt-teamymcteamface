9678a7ceadb3d9ef7a10183a46a8da4ba34a0b2f
Jascha <jascha.wolter@web.de>
2020-03-18 09:06:28 +0100

    Add method signature in IPostRepository and IThreadRepository

--- src/main/java/mops/foren/domain/repositoryabstraction/IThreadRepository.java
+++ src/main/java/mops/foren/domain/repositoryabstraction/IThreadRepository.java
@@ -1,4 +1,12 @@
 package mops.foren.domain.repositoryabstraction;
 
+import mops.foren.domain.model.ThreadId;
+import mops.foren.domain.model.Topic;
+
+import java.util.List;
+
 public interface IThreadRepository {
+    List<Thread> getThreadsFromTopic(Topic topic);
+
+    Thread getThreadById(ThreadId threadId);
 }
