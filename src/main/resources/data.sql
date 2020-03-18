f940ae81167f796a0ad4ec17e238c60fd08b964c
smn_crm <simon.cramer@hhu.de>
2020-03-18 11:34:59 +0100

    Update seed to include more info
    Co-authored by Valentin Stoehr <vali.stoehr@gmail.com>
    Co-authored by Marco Steinke <mastremolten2@gmail.com>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -32,11 +32,56 @@ VALUES (1,
          Bitte besuchen sie regelmäßig die Vorlesung, da es kein Skript geben wird!',
         'Machine Learning');
 
-INSERT INTO `topic` (`id`,`description`,`moderated`,`title`,`forum_id`) VALUES (1,'Hier sind offizielle Ankündigungen und Informationen',1,'Ankündigungen',1);
-INSERT INTO `topic` (`id`,`description`,`moderated`,`title`,`forum_id`) VALUES (2,'Hier kann über Aufgaben und Probleme diskutiert werden',0,'Aufgabenstellungen',1);
-INSERT INTO `topic` (`id`,`description`,`moderated`,`title`,`forum_id`) VALUES (3,'Hier werden nur Memes gepostet',0,'Off-Topic',1);
-INSERT INTO `topic` (`id`,`description`,`moderated`,`title`,`forum_id`) VALUES (4,'Hier sind offizielle Ankündigungen und Informationen',1,'Ankündigungen',2);
-INSERT INTO `topic` (`id`,`description`,`moderated`,`title`,`forum_id`) VALUES (5,'Hier kann über Aufgaben und Probleme diskutiert werden',0,'Aufgabenstellungen',2);
+INSERT
+INTO `topic` (`id`, `description`, `moderated`, `title`, `forum_id`)
+VALUES (1, 'Hier sind offizielle Ankündigungen und Informationen', 1, 'Ankündigungen', 1),
+       (2, 'Hier kann über Aufgaben und Probleme diskutiert werden', 0, 'Aufgabenstellungen', 1),
+       (3, 'Hier werden nur Memes gepostet', 0, 'Off-Topic', 1),
+       (4, 'Hier sind offizielle Ankündigungen und Informationen', 1, 'Ankündigungen', 2),
+       (5, 'Hier kann über Aufgaben und Probleme diskutiert werden', 0, 'Aufgabenstellungen', 2),
+       (6, 'Hier werden nur Memes gepostet', 0, 'Off-Topic', 2),
+       (7, 'Hier sind offizielle Ankündigungen und Informationen', 1, 'Ankündigungen', 3),
+       (8, 'Hier kann über Aufgaben und Probleme diskutiert werden', 0, 'Aufgabenstellungen', 3),
+       (9, 'Hier werden nur Memes gepostet', 0, 'Off-Topic', 3),
+       (10, 'Hier sind offizielle Ankündigungen und Informationen', 1, 'Ankündigungen', 4),
+       (11, 'Hier kann über Aufgaben und Probleme diskutiert werden', 0, 'Aufgabenstellungen', 4),
+       (12, 'Hier werden nur Memes gepostet', 0, 'Off-Topic', 4);
+
+INSERT
+INTO thread (id, title, topic_id)
+VALUES (1, 'Klausurtermine', 1),
+       (2, 'Praktikum: Informationen', 1),
+       (3, 'Organisation / Austausch', 1),
+       (4, 'Tutorenjobs im WS 2020', 1),
+       (5, 'Kommt zum Hackathon !', 1),
+       (6, 'Ich brauche Hilfe bei Aufgabe 4a', 2),
+       (7, 'Was ist eine IDE ?', 2),
+       (8, 'Docker Compose funktioniert nicht :(', 2),
+       (9, 'Ich will lieber Germanistik studieren', 2),
+       (10, 'Was ist Hibernate ?', 2);
+
+
+INSERT
+INTO post (id, author, date_time, thread_id, user_username, text)
+VALUES (1,
+        'Peter Organisatorus',
+        '2020-03-12 14:32:04',
+        1,
+        'orga',
+        'Hallo Zusammen \nDie Klausur findet am 13.07. statt ! Um 13:00 in 5D.'),
+       (2,
+        'Lisa Schmidt',
+        '2020-03-12 15:12:49',
+        1,
+        'studentin1',
+        'Och nöö, da bin ich im Urlaub :('),
+       (3,
+        'Peter Organisatorus',
+        '2020-03-12 16:05:50',
+        1,
+        'orga',
+        'hahahaha, verkackt.');
+
 
 
 INSERT
