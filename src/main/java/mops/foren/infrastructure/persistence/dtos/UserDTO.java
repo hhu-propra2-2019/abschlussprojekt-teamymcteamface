fe99e90ff95cc380aadd1a4d520bb5ba56b645ec
Jascha <jascha.wolter@web.de>
2020-03-19 10:44:34 +0100

    Set some important attributes in the dtos.

--- src/main/java/mops/foren/infrastructure/persistence/dtos/UserDTO.java
+++ src/main/java/mops/foren/infrastructure/persistence/dtos/UserDTO.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 
@@ -29,6 +30,19 @@ public class UserDTO {
             joinColumns = {@JoinColumn(name = "username")},
             inverseJoinColumns = {@JoinColumn(name = "id")})
     private Set<ForumDTO> forums;
+
+    /**
+     * Creates role-table (automatically) and maps roles of user
+     * inside forums in this Map.
+     * Only saves special permissions > student.
+     * Key: ForumId
+     * Value: Role(ID)
+     */
+    @ElementCollection
+    @MapKeyColumn(name = "forumId")
+    @Column(name = "role")
+    @CollectionTable(name = "roles", joinColumns = @JoinColumn(name = "username"))
+    private Map<Long, Long> roles;
 
     @OneToMany(mappedBy = "author")
     private List<PostDTO> posts;
