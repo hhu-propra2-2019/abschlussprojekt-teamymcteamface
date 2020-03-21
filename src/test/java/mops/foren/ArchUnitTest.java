0154b2cb1d8f33cd326d0af9cdace91f8f245bcc
niruc100 <49512133+niruc100@users.noreply.github.com>
2020-03-21 16:06:50 +0100

    add ArchUnit testcase

--- src/test/java/mops/foren/ArchUnitTest.java
+++ src/test/java/mops/foren/ArchUnitTest.java
@@ -1,8 +1,19 @@
 package mops.foren;
 
 import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.context.annotation.SessionScope;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 @AnalyzeClasses(packages = "mops.foren")
 public class ArchUnitTest {
-
+    @ArchTest
+    static final ArchRule controllerCanHandleMultipleUser = classes()
+            .that()
+            .areAnnotatedWith(Controller.class)
+            .should()
+            .beAnnotatedWith(SessionScope.class);
 }
