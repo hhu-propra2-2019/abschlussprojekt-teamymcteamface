13b8d093bfafe8d2a46cb6f011a397b9c7f72355
niruc100 <49512133+niruc100@users.noreply.github.com>
2020-03-22 14:27:27 +0100

    refactor ProfileControllerTest.java and add testcases

--- src/test/java/mops/foren/infrastructure/web/controller/ProfileControllerTest.java
+++ src/test/java/mops/foren/infrastructure/web/controller/ProfileControllerTest.java
@@ -1,6 +1,8 @@
 package mops.foren.infrastructure.web.controller;
 
+import mops.foren.infrastructure.web.Account;
 import mops.foren.infrastructure.web.KeycloakService;
+import mops.foren.infrastructure.web.KeycloakTokenMock;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +13,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.Set;
+
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -40,9 +44,33 @@ public class ProfileControllerTest {
     }
 
     @Test
-    void testProfileTemplate() throws Exception {
+    void testProfileUnauthenticated() throws Exception {
+
+        this.mvcMock.perform(get("/foren/profile"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/sso/login"));
+    }
+
+    @Test
+    void testProfileAuthenticated() throws Exception {
+        KeycloakTokenMock.setupTokenMock(Account.builder()
+                .name("studentin")
+                .roles(Set.of("studentin"))
+                .build());
+
         this.mvcMock.perform(get("/foren/profile"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("profile"));
+    }
+
+    @Test
+    void testProfileWrongUser() throws Exception {
+        KeycloakTokenMock.setupTokenMock(Account.builder()
+                .name("orga1")
+                .roles(Set.of("wrong role"))
+                .build());
+
+        this.mvcMock.perform(get("/foren/profile"))
+                .andExpect(status().isForbidden());
     }
 }
