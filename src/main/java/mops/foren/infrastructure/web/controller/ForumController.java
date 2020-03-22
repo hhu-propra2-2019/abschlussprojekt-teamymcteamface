c2765083d4a4bcb3fba978eb9879e7deb2210978
niruc100 <49512133+niruc100@users.noreply.github.com>
2020-03-22 13:34:16 +0100

    add ThreadController.java and refactoring add a thread functionality

--- src/main/java/mops/foren/infrastructure/web/controller/ForumController.java
+++ src/main/java/mops/foren/infrastructure/web/controller/ForumController.java
@@ -1,9 +1,10 @@
 package mops.foren.infrastructure.web.controller;
 
 import mops.foren.applicationservices.*;
-import mops.foren.domain.model.Thread;
-import mops.foren.domain.model.*;
-import mops.foren.domain.model.paging.PostPage;
+import mops.foren.domain.model.ForumId;
+import mops.foren.domain.model.Post;
+import mops.foren.domain.model.ThreadId;
+import mops.foren.domain.model.User;
 import mops.foren.infrastructure.web.ForumForm;
 import mops.foren.infrastructure.web.PostForm;
 import mops.foren.infrastructure.web.ThreadForm;
@@ -84,27 +85,6 @@ public class ForumController {
         return "forum-mainpage";
     }
 
-
-    /**
-     * Mapping to the thread page.
-     *
-     * @param threadID the thread id
-     * @param model    the model
-     * @return The template for the thread
-     */
-    @GetMapping("/thread")
-    public String displayAThread(@RequestParam("threadId") Long threadID,
-                                 @RequestParam("page") Integer page,
-                                 Model model) {
-        ThreadId threadId = new ThreadId(threadID);
-        PostPage postPage = this.postService.getPosts(threadId, page - 1);
-        model.addAttribute("thread", this.threadService.getThread(threadId));
-        model.addAttribute("posts", postPage.getPosts());
-        model.addAttribute("pagingObject", postPage.getPaging());
-        model.addAttribute("form", this.postForm);
-        return "thread";
-    }
-
     /**
      * Creating a post and redirecting to the thread page.
      *
@@ -126,43 +106,4 @@ public class ForumController {
         return "redirect:/thread?threadId=" + threadIdLong + "&page=" + (page + 1);
     }
 
-    /**
-     * Brings up the form to create a new thread.
-     *
-     * @param forenID The forum id
-     * @param topicID The topic id
-     * @param model   The model
-     * @return The template to create a new thread
-     */
-    @GetMapping("/{forenID}/{topicID}/new-thread")
-    public String createNewThread(@PathVariable Long forenID,
-                                  @PathVariable Long topicID,
-                                  Model model) {
-        model.addAttribute("form", this.threadForm);
-        model.addAttribute("forenId", new ForumId(forenID));
-        model.addAttribute("topicId", new TopicId(topicID));
-        return "create-thread";
-    }
-
-
-    /**
-     * Creates a thread and redirects to the topic page.
-     *
-     * @param token       The user token
-     * @param forenIdLong The forum id
-     * @param topicIdLong The topic id
-     * @param threadForm  The form that that includes the new thread
-     * @return The topic page template
-     */
-    @PostMapping("/forum/newThread")
-    public String addNewThread(KeycloakAuthenticationToken token,
-                               @RequestParam("forenId") Long forenIdLong,
-                               @RequestParam("topicId") Long topicIdLong,
-                               @ModelAttribute ThreadForm threadForm) {
-        User user = this.userService.getUserFromDB(token);
-        TopicId topicId = new TopicId(topicIdLong);
-        Thread thread = threadForm.getThread(user, topicId);
-        this.topicService.addThreadInTopic(topicId, thread);
-        return "redirect:/my-forums/" + forenIdLong + "/" + topicIdLong;
-    }
 }
