9124f61268a88368f9efe163880d876274396317
maste150hhu <mastremolten2@gmail.com>
2020-03-23 12:52:46 +0100

    change CrudRepository to PagingAndSortingRepository

--- src/main/java/mops/foren/infrastructure/persistence/repositories/ThreadJpaRepository.java
+++ src/main/java/mops/foren/infrastructure/persistence/repositories/ThreadJpaRepository.java
@@ -3,10 +3,10 @@ package mops.foren.infrastructure.persistence.repositories;
 import mops.foren.infrastructure.persistence.dtos.ThreadDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ThreadJpaRepository extends CrudRepository<ThreadDTO, Long> {
+public interface ThreadJpaRepository extends PagingAndSortingRepository<ThreadDTO, Long> {
     Page<ThreadDTO> findByTopic_Id(Long id, Pageable pageable);
 }
