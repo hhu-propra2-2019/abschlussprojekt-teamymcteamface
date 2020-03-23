2a7cd9c2a9a2f75b33c5dc6ff37d0ca5cb7fb79d
maste150hhu <mastremolten2@gmail.com>
2020-03-23 10:18:47 +0100

    add paging-method to IThreadRepository

--- src/main/java/mops/foren/domain/repositoryabstraction/IThreadRepository.java
+++ src/main/java/mops/foren/domain/repositoryabstraction/IThreadRepository.java
@@ -4,10 +4,13 @@ import mops.foren.domain.model.Post;
 import mops.foren.domain.model.Thread;
 import mops.foren.domain.model.ThreadId;
 import mops.foren.domain.model.TopicId;
+import mops.foren.domain.model.paging.ThreadPage;
 
 import java.util.List;
 
 public interface IThreadRepository {
+    ThreadPage getThreadPageFromDB(TopicId topicId, Integer page);
+
     List<Thread> getThreadsFromDB(TopicId topicId);
 
     Thread getThreadById(ThreadId threadId);
