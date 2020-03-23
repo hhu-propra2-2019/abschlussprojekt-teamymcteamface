f449ab7bfe16ef438a5a4649b706bcf973ca2b17
Sebastian Jäger <mgtgecko@gmail.com>
2020-03-23 11:00:41 +0100

    removed unnecessary validation for Account fields
    Co-authored-by: arbenber <49586478+arbenber@users.noreply.github.com>

--- src/main/java/mops/foren/infrastructure/web/Account.java
+++ src/main/java/mops/foren/infrastructure/web/Account.java
@@ -4,27 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.util.Set;
 
 @Getter
 @AllArgsConstructor
 @Builder
 public class Account {
-
-    // no check for size because this is not our responsibility
-    @NotNull(message = "Name cannot be Null.")
     private final String name;
-
-    // no check for size because this is not our responsibility
-    @NotNull(message = "Email cannot be Null.")
     private final String email;
-
-    // no checks because image is Null (currently)
     private final String image;
-
-    @NotEmpty(message = "Name cannot be empty.")
     private final Set<String> roles;
-
 }
