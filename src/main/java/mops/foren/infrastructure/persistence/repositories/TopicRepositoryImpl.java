1539fbc627a3a49bb44a27a2c82535157cd90e22
Sebastian Jäger <mgtgecko@gmail.com>
2020-03-24 11:13:22 +0100

    add functionality to save post with topic
    Co-authored-by: arbenber <49586478+arbenber@users.noreply.github.com>

--- src/main/java/mops/foren/infrastructure/persistence/repositories/TopicRepositoryImpl.java
+++ src/main/java/mops/foren/infrastructure/persistence/repositories/TopicRepositoryImpl.java
@@ -5,12 +5,15 @@ import mops.foren.domain.model.Thread;
 import mops.foren.domain.model.Topic;
 import mops.foren.domain.model.TopicId;
 import mops.foren.domain.repositoryabstraction.ITopicRepository;
+import mops.foren.infrastructure.persistence.dtos.PostDTO;
 import mops.foren.infrastructure.persistence.dtos.ThreadDTO;
 import mops.foren.infrastructure.persistence.dtos.TopicDTO;
 import mops.foren.infrastructure.persistence.mapper.ThreadMapper;
 import mops.foren.infrastructure.persistence.mapper.TopicMapper;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -53,11 +56,39 @@ public class TopicRepositoryImpl implements ITopicRepository {
         return TopicMapper.mapTopicDtoToTopic(topicDto);
     }
 
+    /**
+     * This method adds the given thread to the given topic and uses its description
+     * to generate the first post.
+     *
+     * @param topicId the topic in which the new thread should be added
+     * @param thread  the thread to add
+     */
     @Override
     public void addThreadInTopic(TopicId topicId, Thread thread) {
         TopicDTO topicDTO = this.topicRepository.findById(topicId.getId()).get();
-        ThreadDTO threadDTO = ThreadMapper.mapThreadToThreadDto(thread, topicDTO);
+        ThreadDTO threadDTO = addPostDTOToThreadDTO(
+                ThreadMapper.mapThreadToThreadDto(thread, topicDTO));
         topicDTO.getThreads().add(threadDTO);
         this.topicRepository.save(topicDTO);
+    }
+
+    /**
+     * Private helper function which generates a new postDTO from the given topicDTO's
+     * description and adds it to the threadDTO.
+     *
+     * @param threadDTO the threadDTO to which the new postDTO will be added
+     * @return the threadDTO with the new postDTO added to its posts list
+     */
+    private ThreadDTO addPostDTOToThreadDTO(ThreadDTO threadDTO) {
+        PostDTO postDTO = PostDTO.builder()
+                .text(threadDTO.getDescription())
+                .author(threadDTO.getAuthor())
+                .dateTime(LocalDateTime.now())
+                .thread(threadDTO)
+                .build();
+
+        threadDTO.setPosts(Arrays.asList(postDTO));
+
+        return threadDTO;
     }
 }
