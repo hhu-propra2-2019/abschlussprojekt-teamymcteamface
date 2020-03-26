f7e7e524ee5cae4c9262d0916384e3a91b3991e1
sebastian <sebastian.brenke@gmx.de>
2020-03-26 10:26:47 +0100

    Add tests for PermissionManager
    Co-authored-by: Sebastian Brenke <sebastian.brenke@gmx.de>

--- src/test/java/mops/foren/domain/model/PermissionManagerTest.java
+++ src/test/java/mops/foren/domain/model/PermissionManagerTest.java
@@ -1,13 +1,8 @@
 package mops.foren.domain.model;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.config.SetFactoryBean;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PermissionManagerTest {
@@ -15,70 +10,72 @@ class PermissionManagerTest {
     PermissionManager permissionManager;
 
 
+    @BeforeEach
+    void setUp() {
+        //Arrange
+        this.permissionManager = new PermissionManager();
+    }
+
     @Test
     void addForumWithPermissionAddTwoStudents() {
         // Arrange
-        permissionManager = new PermissionManager();
         ForumId Id1 = new ForumId(1L);
         ForumId Id2 = new ForumId(2L);
 
         //Act
-        permissionManager.addForumWithPermission(Id1.getId(), Role.STUDENT);
-        permissionManager.addForumWithPermission(Id2.getId(),Role.STUDENT);
+        this.permissionManager.addForumWithPermission(Id1.getId(), Role.STUDENT);
+        this.permissionManager.addForumWithPermission(Id2.getId(), Role.STUDENT);
 
         //Assert
-        assertThat(permissionManager.getStudent()).containsExactly(Id1,Id2);
+        assertThat(this.permissionManager.getStudent()).containsExactly(Id1, Id2);
     }
 
     @Test
     void addForumWithPermissionAddTwoAdminsOneStudent() {
         // Arrange
-        permissionManager = new PermissionManager();
         ForumId Id1 = new ForumId(5L);
         ForumId Id2 = new ForumId(17L);
         ForumId Id3 = new ForumId(12L);
 
         //Act
-        permissionManager.addForumWithPermission(Id1.getId(), Role.ADMIN);
-        permissionManager.addForumWithPermission(Id2.getId(), Role.ADMIN);
-        permissionManager.addForumWithPermission(Id3.getId(), Role.STUDENT);
+        this.permissionManager.addForumWithPermission(Id1.getId(), Role.ADMIN);
+        this.permissionManager.addForumWithPermission(Id2.getId(), Role.ADMIN);
+        this.permissionManager.addForumWithPermission(Id3.getId(), Role.STUDENT);
 
         //Assert
-        assertThat(permissionManager.getAdmin()).containsExactly(Id1, Id2);
+        assertThat(this.permissionManager.getAdmin()).containsExactly(Id1, Id2);
     }
 
     @Test
     void addForumWithPermissionAddNoModerator() {
         // Arrange
-        permissionManager = new PermissionManager();
         ForumId Id1 = new ForumId(5L);
         ForumId Id2 = new ForumId(17L);
         ForumId Id3 = new ForumId(12L);
 
         //Act
-        permissionManager.addForumWithPermission(Id1.getId(), Role.ADMIN);
-        permissionManager.addForumWithPermission(Id2.getId(), Role.ADMIN);
-        permissionManager.addForumWithPermission(Id3.getId(), Role.STUDENT);
+        this.permissionManager.addForumWithPermission(Id1.getId(), Role.ADMIN);
+        this.permissionManager.addForumWithPermission(Id2.getId(), Role.ADMIN);
+        this.permissionManager.addForumWithPermission(Id3.getId(), Role.STUDENT);
 
         //Assert
-        assertThat(permissionManager.getModerator()).isEmpty();
+        assertThat(this.permissionManager.getModerator()).isEmpty();
     }
 
     @Test
     void addForumWithPermissionAddOneModerator() {
         // Arrange
-        permissionManager = new PermissionManager();
         ForumId Id1 = new ForumId(5L);
         ForumId Id2 = new ForumId(17L);
         ForumId Id3 = new ForumId(12L);
 
         //Act
-        permissionManager.addForumWithPermission(Id1.getId(), Role.MODERATOR);
-        permissionManager.addForumWithPermission(Id2.getId(), Role.ADMIN);
-        permissionManager.addForumWithPermission(Id3.getId(), Role.STUDENT);
+        this.permissionManager.addForumWithPermission(Id1.getId(), Role.MODERATOR);
+        this.permissionManager.addForumWithPermission(Id2.getId(), Role.ADMIN);
+        this.permissionManager.addForumWithPermission(Id3.getId(), Role.STUDENT);
 
         //Assert
-        assertThat(permissionManager.getModerator()).containsExactly(Id1);
+        assertThat(this.permissionManager.getModerator()).containsExactly(Id1);
     }
 
     // After establishing, that the addForumWithPermission function works, we can use this function in the arrage
@@ -91,9 +88,52 @@ class PermissionManagerTest {
 
     @Test
     void testCheckPermission() {
+
     }
 
     @Test
-    void getAllForums() {
+    void getAllForumsOnlyStudents() {
+        //Arrange
+        ForumId forumId1 = new ForumId(1L);
+        ForumId forumId2 = new ForumId(2L);
+        ForumId forumId3 = new ForumId(3L);
+
+        //Act
+        this.permissionManager.addForumWithPermission(forumId1.getId(), Role.STUDENT);
+        this.permissionManager.addForumWithPermission(forumId2.getId(), Role.STUDENT);
+        this.permissionManager.addForumWithPermission(forumId3.getId(), Role.STUDENT);
+
+        //Assert
+        assertThat(this.permissionManager.getAllForums()).containsExactlyInAnyOrder(
+                forumId1, forumId2, forumId3);
     }
+
+    @Test
+    void getAllForumsDuplicate() {
+        //Arrange
+        ForumId forumId1 = new ForumId(1L);
+        ForumId forumId2 = new ForumId(2L);
+        ForumId forumId3 = new ForumId(3L);
+
+        //Act
+        this.permissionManager.addForumWithPermission(forumId1.getId(), Role.STUDENT);
+        this.permissionManager.addForumWithPermission(forumId1.getId(), Role.MODERATOR);
+        this.permissionManager.addForumWithPermission(forumId2.getId(), Role.MODERATOR);
+        this.permissionManager.addForumWithPermission(forumId2.getId(), Role.ADMIN);
+        this.permissionManager.addForumWithPermission(forumId3.getId(), Role.STUDENT);
+        this.permissionManager.addForumWithPermission(forumId3.getId(), Role.MODERATOR);
+
+
+        //Assert
+        assertThat(this.permissionManager.getAllForums()).containsExactlyInAnyOrder(
+                forumId1, forumId2, forumId3);
+    }
+
+    @Test
+    void getAllForumsEmpty() {
+        //Assert
+        assertThat(this.permissionManager.getAllForums()).isEmpty();
+    }
+
+
 }
