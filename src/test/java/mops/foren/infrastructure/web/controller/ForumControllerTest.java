64d3638926950549c514adfb736a9481998ee3bd
niruc100 <49512133+niruc100@users.noreply.github.com>
2020-03-27 20:50:37 +0100

    add testcase and refactor old cases

--- src/test/java/mops/foren/infrastructure/web/controller/ForumControllerTest.java
+++ src/test/java/mops/foren/infrastructure/web/controller/ForumControllerTest.java
@@ -4,7 +4,10 @@ import mops.foren.applicationservices.ForumService;
 import mops.foren.applicationservices.PostService;
 import mops.foren.applicationservices.TopicService;
 import mops.foren.applicationservices.UserService;
-import mops.foren.domain.model.*;
+import mops.foren.domain.model.Forum;
+import mops.foren.domain.model.User;
+import mops.foren.domain.model.paging.Paging;
+import mops.foren.domain.model.paging.PostPage;
 import mops.foren.infrastructure.web.Account;
 import mops.foren.infrastructure.web.KeycloakService;
 import mops.foren.infrastructure.web.KeycloakTokenMock;
@@ -19,6 +22,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -51,6 +55,9 @@ public class ForumControllerTest {
 
     @MockBean
     TopicService topicServiceMock;
+
+    @MockBean
+    User userMock;
 
     /**
      * Building up a security environment for the Test.
@@ -105,12 +112,10 @@ public class ForumControllerTest {
                 .name("orga")
                 .roles(Set.of("orga"))
                 .build();
-        User fakeUser = User.builder().name("orga").build();
         KeycloakTokenMock.setupTokenMock(fakeAccount);
 
-
-        when(userServiceMock.getUserFromDB(any())).thenReturn(fakeUser);
-        when(forumServiceMock.getForums(fakeUser)).thenReturn(new LinkedList<>());
+        when(userServiceMock.getUserFromDB(any())).thenReturn(userMock);
+        when(forumServiceMock.getForums(any())).thenReturn(new LinkedList<>());
 
         this.mvcMock.perform(get("/foren"))
                 .andExpect(status().isOk())
@@ -119,19 +124,14 @@ public class ForumControllerTest {
 
     @Test
     void testEnterAForumWithoutPermission() throws Exception {
-        //Arrange
         Account fakeAccount = Account.builder()
                 .name("studentin")
                 .roles(Set.of("studentin"))
                 .build();
         KeycloakTokenMock.setupTokenMock(fakeAccount);
 
-        User fakeUser = User.builder()
-                .name("studentin")
-                .permissionManager(new PermissionManager())
-                .build();
-
-        when(userServiceMock.getUserFromDB(any())).thenReturn(fakeUser);
+        when(userServiceMock.getUserFromDB(any())).thenReturn(userMock);
+        when(userMock.checkPermission(any(), any())).thenReturn(false);
 
         this.mvcMock.perform(get("/foren/my-forums/enter?forumId=1"))
                 .andExpect(status().isOk())
@@ -147,16 +147,10 @@ public class ForumControllerTest {
                 .build();
         KeycloakTokenMock.setupTokenMock(fakeAccount);
 
-        User fakeUser = User.builder()
-                .name("studentin")
-                .permissionManager(new PermissionManager())
-                .build();
-
-        fakeUser.getPermissionManager().addForumWithPermission(1L, Role.STUDENT);
         Forum fakeForum = Forum.builder().title("").build();
-
-        when(userServiceMock.getUserFromDB(any())).thenReturn(fakeUser);
-        when(forumServiceMock.getForum(new ForumId(1L))).thenReturn(fakeForum);
+        when(userServiceMock.getUserFromDB(any())).thenReturn(userMock);
+        when(userMock.checkPermission(any(), any())).thenReturn(true);
+        when(forumServiceMock.getForum(any())).thenReturn(fakeForum);
 
         this.mvcMock.perform(get("/foren/my-forums/enter?forumId=1"))
                 .andExpect(status().isOk())
@@ -172,43 +166,53 @@ public class ForumControllerTest {
                 .build();
         KeycloakTokenMock.setupTokenMock(fakeAccount);
 
-        User fakeUser = User.builder()
-                .name("studentin")
-                .permissionManager(new PermissionManager())
-                .build();
-
-        fakeUser.getPermissionManager().addForumWithPermission(1L, Role.STUDENT);
         Forum fakeForum = Forum.builder().title("").build();
-
-        when(userServiceMock.getUserFromDB(any())).thenReturn(fakeUser);
+        when(userServiceMock.getUserFromDB(any())).thenReturn(userMock);
+        when(userMock.checkPermission(any(), any())).thenReturn(true);
         when(forumServiceMock.getForum(any())).thenReturn(fakeForum);
 
         this.mvcMock.perform(get("/foren/my-forums/enter?forumId=1"))
                 .andExpect(model().attribute("forum", fakeForum))
                 .andExpect(model().attribute("forumId", 1L))
-                .andExpect(model().attribute("createPermission", false))
-                .andExpect(model().attribute("permission", false));
+                .andExpect(model().attribute("createPermission", true))
+                .andExpect(model().attribute("permission", true));
     }
 
     @Test
     void testSearchAForumWithoutPermission() throws Exception {
-        //Arrange
         Account fakeAccount = Account.builder()
                 .name("studentin")
                 .roles(Set.of("studentin"))
                 .build();
         KeycloakTokenMock.setupTokenMock(fakeAccount);
 
-        User fakeUser = User.builder()
-                .name("studentin")
-                .permissionManager(new PermissionManager())
-                .build();
-
-        when(userServiceMock.getUserFromDB(any())).thenReturn(fakeUser);
+        when(userServiceMock.getUserFromDB(any())).thenReturn(userMock);
+        when(userMock.checkPermission(any(), any())).thenReturn(false);
 
         this.mvcMock.perform(get("/foren/my-forums/search?forumId=1&searchContent=hallo&page=0"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("error-no-permission"));
+    }
+
+    @Test
+    void testSearchWithPermission() throws Exception {
+        //Arrange
+        Account fakeAccount = Account.builder()
+                .name("orga")
+                .roles(Set.of("orga"))
+                .build();
+        KeycloakTokenMock.setupTokenMock(fakeAccount);
+
+        Paging paging = new Paging(true, true, false, 0, 0L, 0);
+        PostPage postPage = new PostPage(paging, List.of());
+
+        when(userServiceMock.getUserFromDB(any())).thenReturn(userMock);
+        when(userMock.checkPermission(any(), any())).thenReturn(true);
+        when(postServiceMock.searchWholeForum(any(), any(), any())).thenReturn(postPage);
+
+        this.mvcMock.perform(get("/foren/my-forums/search?forumId=1&searchContent=hallo&page=0"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("search-result-posts"));
     }
 }
 
