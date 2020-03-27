698464bb605c1edf786d5889a77e1987c12d7fe0
niruc100 <49512133+niruc100@users.noreply.github.com>
2020-03-27 10:10:58 +0100

    fix typo

--- src/main/java/mops/foren/infrastructure/web/controller/ForumController.java
+++ src/main/java/mops/foren/infrastructure/web/controller/ForumController.java
@@ -58,7 +58,7 @@ public class ForumController {
      * Method checks if user is in the DB and gets all his forums.
      *
      * @param token Keycloak token.
-     * @param model Content we add to html per thymleaf.
+     * @param model Content we add to html per Thymleaf.
      * @return Get-mapping for my-forums.
      */
     @GetMapping
