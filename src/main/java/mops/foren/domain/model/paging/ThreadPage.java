50faa831b6fe5f82c8311dfd905f9bbea8b17079
Jascha <jascha.wolter@web.de>
2020-03-28 18:04:03 +0100

    Add missing javadoc comment.

--- src/main/java/mops/foren/domain/model/paging/ThreadPage.java
+++ src/main/java/mops/foren/domain/model/paging/ThreadPage.java
@@ -12,6 +12,12 @@ public class ThreadPage {
     Paging paging;
     List<Thread> threads;
 
+
+    /**
+     * This method count the Posts that are not visible form the thread page.
+     *
+     * @return A count of all invisible posts.
+     */
     public Long getCountUnmoderatedPosts() {
         return threads.stream()
                 .filter(thread -> thread.getUnModerated() != null)
