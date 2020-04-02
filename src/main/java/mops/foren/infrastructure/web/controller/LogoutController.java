4a5aa0ff9263696d7ed48c7233c584522e7ba88f
sebastian <mgtgecko@gmail.com>
2020-04-02 18:29:06 +0200

    fix logout route to work with mops layout

--- src/main/java/mops/foren/infrastructure/web/controller/LogoutController.java
+++ src/main/java/mops/foren/infrastructure/web/controller/LogoutController.java
@@ -2,14 +2,12 @@ package mops.foren.infrastructure.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.annotation.SessionScope;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @SessionScope
-@RequestMapping("/foren")
 public class LogoutController {
 
     @GetMapping("/logout")
